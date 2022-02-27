995819352974bc584c282db151a3006f527043b5
4lRZ <adormundo@gmail.com>
2022-02-27 19:58:37 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -16,6 +16,10 @@ public class ChessPosition {
 
     }
 
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
     public char getColumn() {
         return column;
     }
@@ -26,10 +30,6 @@ public class ChessPosition {
 
     protected Position toPosition() {
         return new Position(8 - row, column - 'a');
-    }
-
-    protected static ChessPosition fromPosition(Position position) {
-        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
     }
 
     @Override
